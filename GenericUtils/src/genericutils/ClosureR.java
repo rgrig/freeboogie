d9fd47fdf033583ccb25bf2dbf81f71453b48ce6
radugrigore <radugrigore@6b153c96-75e8-11de-b098-dd011fd6ff08>
2009-07-27 21:53:07 +0000

    Added StringUtil to repo.
    This file is rather old but I keep forgrtting to put it in svn.

--- GenericUtils/src/genericutils/ClosureR.java
+++ GenericUtils/src/genericutils/ClosureR.java
@@ -2,11 +2,13 @@ package genericutils;
 
 /**
  * For functions that take {@code P} and return {@code R}.
+ * DEPRECATED: Use com.google.common.base.Function instead.
  *
  * @author rgrig 
  * @param <P> the type of the parameter
  * @param <R> teh type of the result
  */
+@Deprecated
 public abstract class ClosureR<P, R> {
   /**
    * Process {@code p}.
