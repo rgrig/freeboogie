e9c5e79cd4ac25cfaeb462263212c23e5fb5e624
radugrigore <radugrigore@6b153c96-75e8-11de-b098-dd011fd6ff08>
2009-09-18 13:14:11 +0000

    If desugarer (and a few tests).
    Fixes issue 35.

--- trunk/FreeBoogie/src/freeboogie/vcgen/IfDesugarer.java
+++ trunk/FreeBoogie/src/freeboogie/vcgen/IfDesugarer.java
@@ -1,5 +1,10 @@
 package freeboogie.vcgen;
 
+import com.google.common.collect.ImmutableList;
+import genericutils.Id;
+
+import freeboogie.ast.*;
+
 /** Replaces <b>if</b> statements by <b>assume</b> statements.
   The code
     <pre>
@@ -24,4 +29,39 @@ package freeboogie.vcgen;
   <p>The <b>if</b> statements in Y and N are processed first.
  */
 public class IfDesugarer extends CommandDesugarer {
+  @Override public IfCmd eval(IfCmd cmd) {
+    Block yes = (Block) cmd.yes().eval(this);
+    Block no = Block.mk(ImmutableList.<Command>of(), cmd.loc());
+    if (cmd.no() != null) no = (Block) cmd.no().eval(this);
+    String l1 = Id.get("if");
+    String l2 = Id.get("if");
+    String l3 = Id.get("if");
+    addEquivalentCommand(GotoCmd.mk(
+        cmd.labels(), 
+        ImmutableList.of(l1, l2),
+        cmd.loc()));
+    addEquivalentCommand(AssertAssumeCmd.mk(
+        ImmutableList.of(l1),
+        AssertAssumeCmd.CmdType.ASSUME,
+        AstUtils.ids(),
+        cmd.condition().clone(),
+        cmd.loc()));
+    for (Command c : yes.commands())
+      addEquivalentCommand(c);
+    addEquivalentCommand(AssertAssumeCmd.mk(
+        ImmutableList.of(l2),
+        AssertAssumeCmd.CmdType.ASSUME,
+        AstUtils.ids(),
+        UnaryOp.mk(UnaryOp.Op.NOT, cmd.condition()),
+        cmd.loc()));
+    for (Command c : no.commands())
+      addEquivalentCommand(c);
+    addEquivalentCommand(AssertAssumeCmd.mk(
+        ImmutableList.of(l3),
+        AssertAssumeCmd.CmdType.ASSUME,
+        AstUtils.ids(),
+        BooleanLiteral.mk(BooleanLiteral.Type.TRUE, cmd.loc()),
+        cmd.loc()));
+    return null;
+  }
 }
