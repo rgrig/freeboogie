024cb60491ddea0cee66b28dfb105dece88f36f3
radugrigore <radugrigore@6b153c96-75e8-11de-b098-dd011fd6ff08>
2009-09-20 13:20:24 +0000

    Desugar 'while' statements.
    Not yet tested.
    Fixes issue 33.

--- FreeBoogie/src/freeboogie/vcgen/WhileDesugarer.java
+++ FreeBoogie/src/freeboogie/vcgen/WhileDesugarer.java
@@ -1,38 +1,44 @@
 package freeboogie.vcgen;
 
 import com.google.common.collect.ImmutableList;
+import genericutils.Id;
 
 import freeboogie.ast.*;
 
 /** Desugars <b>while</b> statements.
   The code
     <pre>
-    L: while (C) invariant I... { B }
+    [L:] while (C) invariant I... { B }
     </pre>
   becomes
     <pre>
     L: assume true;
-    assert I;...
+    assert/assume I;...
     if (C) {
       B;
       assert I;...
       goto L;
     }
     </pre>
-  The <tt>assert I</tt> sequence appears twice so that {@code
-  LoopCutter} is complete (when I is strong enough) without much
-  work. 
-
-  TODO: add labels L if not existing
   
-  The body B is processed first.
+  <p>The <tt>assert/assume I</tt> line stands for a sequence of
+  commands that correspond to all the loop invariants. The free
+  ones appear as assumptions, the others appear as assertions.
+  The line <tt>assert I</tt> is a sequence of commands that
+
+  <p>The label L is added if it is missing.
+
+  <p>The body B is processed first.
  */
 public class WhileDesugarer extends CommandDesugarer {
   @Override public WhileCmd eval(WhileCmd cmd) {
-    String l = cmd.labels().get(0);
+    ImmutableList<String> labels = cmd.labels().isEmpty()?
+        ImmutableList.of(Id.get("while")) :
+        cmd.labels();
+    String l = labels.get(0);
     Block body = (Block) cmd.body().eval(this);
     addEquivalentCommand(AssertAssumeCmd.mk(
-        cmd.labels(),
+        labels,
         AssertAssumeCmd.CmdType.ASSUME,
         AstUtils.ids(),
         BooleanLiteral.mk(BooleanLiteral.Type.TRUE, cmd.loc()),
