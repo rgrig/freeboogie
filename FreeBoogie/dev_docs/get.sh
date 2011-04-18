3fc06cd87af9aa500ea4b69693baef2c34dd1d5b
radugrigore <radugrigore@gmail.com>
2011-04-18 10:50:15 +0000

    Download deps from Net.
    FreeBoogie's libraries are fetched automatically if they are missing
    from lib/. They are not needed in the repo anymore.

--- FreeBoogie/dev_docs/get.sh
+++
@@ -1,4 +0,0 @@
-wget http://combination.cs.uiowa.edu/smtlib/papers/smt-lib-reference-v2.0-r10.12.21.pdf
-wget http://research.microsoft.com/en-us/um/people/leino/papers/krml178.pdf
-wget http://pm.ethz.ch/projects/student_docs/Samuel_Willimann/Samuel_Willimann_MA_paper.pdf
-
